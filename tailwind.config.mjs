61644cb31b97761d67b73ffda18680a84c172df1
Adam Wills <adam@adamwills.com>
2024-10-18 12:04:24 -0400

    web version

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-green": "#c3ffbe",
+        "custom-blue-100": "#f1f9ff",
+        "custom-blue-200": "#e3f3fc",
+        "custom-blue-300": "#d9efff",
+        "custom-blue-dark": "#105583",
+      },
+    },
+  },
+  plugins: [],
+};
